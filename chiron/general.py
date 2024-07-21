f39c31e8ad33ced45f92d41c98fe10a86f2a3713
riasc <richard.schaefer@zoho.com>
2024-07-21 18:17:04 -0500

    determine paths of datafiles

---
+++ chiron/general.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+class DataFiles:
+    def __init__(self, input, synthetic):
+        self.he_survey = {} # health and exposure survey data
+        self.he_survey["train"] = self.get_he_surveyfile(input, "train", synthetic)
+        self.he_survey["val"] = self.get_he_surveyfile(input, "val", synthetic)
+
+        self.sv_data = {} # structural variant data
+        self.sv_data["train"] = self.get_sv_datafile(input, "train", synthetic)
+        self.sv_data["val"] = self.get_sv_datafile(input, "val", synthetic)
+
+    def get_he_surveyfile(self, input, type, synthetic):
+        if synthetic:
+            data = Path(input) / Path(type + "_data_synthetic")
+        else:
+            data = Path(input) / Path(type + "_data")
+        surveypath = data / Path("PEGS_freeze_v3.1_nonpii") / Path("Surveys") / Path("Health_and_Exposure")
+        if synthetic:
+            return surveypath / Path("healthexposure_16jun22_v3.1_nonpii_" + type + "_synthetic.RData")
+        else:
+            return surveypath / Path("healthexposure_16jun22_v3.1_nonpii_" + type + ".RData")
+
+    def get_sv_datafile(self, input, type, synthetic):
+        if synthetic:
+            data = Path(input) / Path(type + "_data_synthetic")
+        else:
+            data = Path(input) / Path(type + "_data")
+        svpath = data / Path("PEGS_genomic_data") / Path("structural_variants")
+        if synthetic:
+            return svpath / Path("PEGS_SV_genotypes_" + type + "_synthetic.vcf.gz")
+        else:
+            return svpath / Path("PEGS_SV_genotypes_" + type + ".vcf.gz")
