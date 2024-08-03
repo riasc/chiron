a33fd55c9881628341b708f6765188a44e09fee9
riasc <richard.schaefer@zoho.com>
2024-08-03 13:17:12 -0500

    added local ancestry

--- chiron/genomics.py
+++ chiron/genomics.py
@@ -2,11 +2,20 @@ import pandas as pd
 
 class Telomere:
     def __init__(self, xlsxfile, type):
-        print()
         self.data = self.parse_xlsx(xlsxfile, type)
 
     def parse_xlsx(self, xlsxfile, type):
         df = pd.read_excel(xlsxfile)
         # replace column names
         df.columns = ["epr_number", "sampleID", "telomere_content"]
+        return df
+
+class Ancestry:
+    def __init__(self, xlsxfile, type):
+        self.data = self.parse_xlsx(xlsxfile, type)
+
+    def parse_xlsx(self, xlsxfile, type):
+        df = pd.read_excel(xlsxfile)
+        # replace column names
+        df.columns = ["epr_number", "sampleID", "ancestry_AFR", "ancestry_AMR", "ancestry_EAS", "ancestry_EUR", "ancestry_SAS"]
         return df
